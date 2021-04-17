b754d01f65aec589f6747fbef8ba1b5141c8b22d
Thomas 'tob' Blochet <tbsx@outlook.fr>
2021-04-17 02:17:00 +0200

    [refacto] change path

--- design_table/design_table/urls.py
+++ design_table/design_table/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('display/', include('design_display.urls')),
+    path('design/', include('design_display.urls')),
     path('admin/', admin.site.urls),
 ]
