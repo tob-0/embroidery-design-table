5b426784b2da34643138930e7ede533692d8e377
Thomas 'tob' Blochet <tbsx@outlook.fr>
2021-04-17 02:17:21 +0200

    [feat] new views

--- design_table/design_display/views.py
+++ design_table/design_display/views.py
@@ -1,6 +1,6 @@
 from django.template import loader
 from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
 from .models import CurrentImage
 
 # Create your views here.
@@ -12,4 +12,17 @@ def index(request):
         'current_image': current_image,
     }
     return HttpResponse(template.render(context,request))
-    
+
+def change(request):
+    current_url = CurrentImage.objects.all().first()
+    context = {
+        'current_url': current_url,
+    }
+    return HttpResponse(render(request,'display/change.html',context))
+
+
+def post(request, ):
+    if request.method == 'POST':
+        new_url = request.POST['new_url']
+        CurrentImage.objects.filter().update(url=new_url)
+    return redirect('/design')
